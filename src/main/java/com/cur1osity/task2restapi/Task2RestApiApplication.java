92110eec678b704a4ccea72c4ba8b50b179951af
cur1osity <marcin.gajc@gmail.com>
2017-12-28 17:05:48 +0100

    First commit

---
+++ src/main/java/com/cur1osity/task2restapi/Task2RestApiApplication.java
@@ -0,0 +1,12 @@
+package com.cur1osity.task2restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task2RestApiApplication.class, args);
+	}
+}
